76c22703cc56fee5ef360a2aecf3541ffbc17108
Emilien-T <emilien.taisne@mail.mcgill.ca>
2023-11-23 14:46:26 -0500

    optimised algorithm

--- FinalProject/path_finding.py
+++ FinalProject/path_finding.py
@@ -14,7 +14,7 @@ fireLocations = []
 fireMatrix = []
 
 #=-=-=-=-=-=- Main path finding function -=-=-=-=-=-=#
-def getInstructionList(coordinatesArray):
+def getDepInstructionList(coordinatesArray):
 	global listOfInstruction
 	global fireLocations
 	global localPosition
@@ -31,6 +31,60 @@ def getInstructionList(coordinatesArray):
 	goToOrigin(localPosition)
 
 	return listOfInstruction
+
+def getInstructionList(coordinatesArray):
+	global listOfInstruction
+	global fireLocations
+	global localPosition
+
+	all_paths = []
+
+	#fireLocation is used to keep track which fire we took care of
+	#coordinatesArray is used to keep a list of all the fires
+	
+	fireOrders = [
+	[fireLocations[0],fireLocations[1],fireLocations[2]],
+	[fireLocations[0],fireLocations[2],fireLocations[1]],
+	[fireLocations[1],fireLocations[0],fireLocations[2]],
+	[fireLocations[1],fireLocations[2],fireLocations[0]],
+	[fireLocations[2],fireLocations[0],fireLocations[1]],
+	[fireLocations[2],fireLocations[1],fireLocations[0]]]
+
+	for fireOrder in fireOrders:
+		initialization(coordinatesArray)
+		
+		for fire in fireOrder:
+			localPosition = goToFire(localPosition, fire)
+
+		goToOrigin(localPosition)
+
+		all_paths.append(listOfInstruction)
+
+	shortestPath = all_paths[0]
+	for path in all_paths:
+		if len(shortestPath) > len(path):
+			shortestPath = path
+
+	return shortestPath
+
+def getShorterInstructionList(coordinatesArray):
+	global listOfInstruction
+	global fireLocations
+	global localPosition
+
+	#fireLocation is used to keep track which fire we took care of
+	#coordinatesArray is used to keep a list of all the fires
+	initialization(coordinatesArray)
+	
+	localPosition = goToFiresWithBbfs(localPosition)
+
+	if not localPosition:
+		print("fire not reachable")
+		return False
+
+	goToOrigin(localPosition)
+
+	return listOfInstruction	
 #=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
 
 #=-=-=-=-=-=-=-=-= Helper functions =-=-=-=-=-=-=-=-=#
@@ -72,6 +126,23 @@ def is_valid_move(x, y, visited):
 	global fireMatrix
 	return 0 <= x < 4 and 0 <= y < 4 and fireMatrix[x][y] == 0 and not visited[y][x]
 
+def check_if_arrived_at_target(x,y, targets, targets_passed):
+	for targetPassedCoord in targets_passed:
+		if x == targetPassedCoord[0] and y == targetPassedCoord[1]:
+			return False
+	for targetCoord in targets:
+		if x == targetCoord[0] and y == targetCoord[1]:
+			return targetCoord
+
+	return False
+
+def has_targets_all_been_passed(passed_targets, targets):
+	for target in targets:
+		if not(targets in passed_targets):
+			return False
+	return True
+		
+
 def bfs(grid, start_x, start_y, target_x, target_y):
 	visited = [[False for _ in range(4)] for _ in range(4)]
 	queue = deque([(start_x, start_y, [])])  # (x, y, path)
@@ -90,6 +161,39 @@ def bfs(grid, start_x, start_y, target_x, target_y):
 				queue.append((new_x, new_y, new_path))
 
 	return []  # Target is not reachable
+
+#targets should be in the following format: [[target1_x, target1_y], [target2_x, target2_y], etc...]
+def bbfs(grid, start_x, start_y, targets):
+	#queue = deque([(start_x, start_y, [], [])])  # (x, y, path, targets_passed)
+	#num_targets = 0
+	next_queue = [(start_x, start_y, [], [])]
+
+	while len(next_queue) > 0 and len(targets) > len(next_queue[0][3]):
+		queue = deque([next_queue.pop(0)])
+		visited = [[False for _ in range(4)] for _ in range(4)]
+		#next_queue = []
+		while queue:
+			x, y, path, targets_passed = queue.popleft()
+			#print(x, y, path, targets_passed)
+			visited[y][x] = True
+
+			is_at_target = check_if_arrived_at_target(x,y,targets, targets_passed)
+			if is_at_target:
+				new_passed = targets_passed[:]
+				new_passed.append(is_at_target)
+				new_path = path + [(x, y)]
+				x,y = path[-1]
+				next_queue.append((x,y,new_path,new_passed))
+			
+			else:
+				for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+					new_x, new_y = x + dx, y + dy
+					if is_valid_move(new_x, new_y, visited) or check_if_arrived_at_target(new_x,new_y,targets, targets_passed):
+						new_path = path + [(x, y)]
+						queue.append((new_x, new_y, new_path, targets_passed))
+		#num_targets += 1
+	return next_queue
+
 
 def goToFire(position, fireCoordinate):
 	global listOfInstruction
@@ -114,6 +218,42 @@ def goToFire(position, fireCoordinate):
 	listOfInstruction.append("creepBWD")
 	return position
 
+def goToFiresWithBbfs(position):
+	global listOfInstruction
+	global fireMatrix
+
+	paths = bbfs(fireMatrix, localPosition[0], localPosition[1], fireLocations)
+
+	if len(paths) == 0:
+		return False
+
+	#finding shortest path
+	shortestPath = paths[0]
+	for path in paths:
+		if len(shortestPath[2]) > len(path[2]):
+			shortestPath = path
+	shortestPath = shortestPath[2]
+	shortestPath.pop(0)
+
+	# print(shortestPath)
+	for coordinate in shortestPath:
+		isFire = False
+		for fireCoordinate in fireLocations:
+			# print(coordinate, fireCoordinate)
+			if coordinate[0] == fireCoordinate[0] and coordinate[1] == fireCoordinate[1]:
+				rotateTowards(position, fireCoordinate)
+				#Instructions to put out the fire
+				listOfInstruction.append("creepFWD")
+				listOfInstruction.append(fireCoordinate[2])
+				listOfInstruction.append("creepBWD")
+				isFire = True
+		if position != coordinate and not isFire and (coordinate != (0,0)):
+			rotateTowards(position, coordinate)
+			listOfInstruction.append("FWD")
+			position = coordinate
+
+	return position
+
 def goToOrigin(position):
 	path = bfs(fireMatrix, localPosition[0], localPosition[1], 0, 0)
 	path.append((0,0))
@@ -124,7 +264,7 @@ def goToOrigin(position):
 			listOfInstruction.append("FWD")
 			position = coordinate
 
-	rotateTowards(position, [0,1])
+	# rotateTowards(position, [0,1])
 
 def ortDistance(coordinate1, coordinate2):
 	return abs(coordinate1[0] - coordinate2[0]) + abs(coordinate1[1] - coordinate2[1])
@@ -168,4 +308,27 @@ def rotateTowards(position, destination):
 				else:
 					listOfInstruction.append("left")
 					localRotation -= 90
-#=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+
+#=-=-=-=-=-=-=-=-=-=- Debugging -=-=-=-=-=-=-=-=-=-=#
+if __name__ == "__main__":
+	print(getShorterInstructionList([[2, 0, 'red'], [2, 1, 'red'], [1, 1, 'red']]))
+	print("=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+	print(getTAInstructionList([[2, 0, 'red'], [2, 1, 'red'], [1, 1, 'red']]))
+	print("=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+	print(getInstructionList([[2, 0, 'red'], [2, 1, 'red'], [1, 1, 'red']]))
+
+	# initialization([[2, 1, 'red'], [3, 2, 'red'], [3, 3, 'red']])
+
+	# results = bbfs(fireMatrix,0,0,fireLocations)
+
+	# mini = results[0]
+	# for result in results:
+	# 	if len(mini[2]) > len(result[2]):
+	# 		mini = result
+
+	# print(mini)
+	
+	# for result in results:
+	# 	print("=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+	# 	print(result)
--- FinalProject/testing/path_finding_test.py
+++ FinalProject/testing/path_finding_test.py
@@ -18,20 +18,43 @@ for i in range(1, len(total_array)-2):
 		for k in range(j+1, len(total_array)):
 			all_selections.append([total_array[i],total_array[j],total_array[k]])
 
-total_len = 0
 total_num = 0
+
+#original stats
+total_len = 0
 worst_path_and_cooridates = [[],[]]
+
+#new stats
+new_total_len = 0
+new_worst_path_and_cooridates = [[],[]]
+
+#new stats
+ta_total_len = 0
+ta_worst_path_and_cooridates = [[],[]]
+
 for coordinates_fire in all_selections:
 	print(coordinates_fire)
 	path = path_finding.getInstructionList(coordinates_fire)
-	if path:
+	shortPath = path_finding.getShorterInstructionList(coordinates_fire)
+	TA_path = path_finding.getTAInstructionList(coordinates_fire)
+	if shortPath:
 		total_num += 1
 		total_len += len(path)
+		new_total_len += len(shortPath)
+		ta_total_len += len(TA_path)
+		if len(ta_worst_path_and_cooridates[1]) < len(TA_path):
+			ta_worst_path_and_cooridates = [coordinates_fire, TA_path]
 		if len(worst_path_and_cooridates[1]) < len(path):
 			worst_path_and_cooridates = [coordinates_fire, path]
+		if len(new_worst_path_and_cooridates[1]) < len(shortPath):
+			new_worst_path_and_cooridates = [coordinates_fire, shortPath]
 
 print("number of fires:", len(all_selections))
 print("number of possible fires:", total_num)
-print("average number of instructions:", total_len/total_num)
-print("worst_path_and_cooridates:", worst_path_and_cooridates)
+print("average number of instructions for old algorithm:", total_len/total_num)
+print("average number of instructions for new algorithm:", new_total_len/total_num)
+print("average number of instructions for TA algorithm:", ta_total_len/total_num)
+print("worst path:", worst_path_and_cooridates)
+print("new worst path:", new_worst_path_and_cooridates)
+print("TA worst path:", ta_worst_path_and_cooridates)
 
